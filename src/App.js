c948c6fdefba709f65ba7fc8c2615600b688cc06
Haley Seymour <haleycseymour@comcast.net>
2022-08-15 20:48:28 -0700

    basic styling and layout for game play

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Let's Play Trivia!</h1>
+        <nav>
+          <ul>
+            <li><a>About</a></li>
+            <li><a>Play</a></li>
+            <li><a>Scoreboard</a></li>
+            <li><a>Trivia Near Me</a></li>
+          </ul>
+        </nav>
       </header>
+      <body>
+        <section class='gameplay'>
+          <p>Game play</p>
+        </section>
+        <aside class='clockscore'>
+          <p>Clock and scoreboard</p>
+        </aside>
+      </body>
+      <footer>
+        <p>Thanks for visiting!</p>
+      </footer>
     </div>
   );
 }
